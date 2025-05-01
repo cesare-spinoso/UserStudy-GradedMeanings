18fe795fcd397226de1a03c10f6cb349fde0766e
cesare-spinoso <cesare.spinoso@gmail.com>
2025-05-01 11:44:26 -0400

    update list

--- using_model_theory_pilot/js/stimuli.js
+++ using_model_theory_pilot/js/stimuli.js
@@ -1,34 +1,36 @@
 // data for all stimuli in the form of a list of JavaScript objects
 
 var all_stims = [
-    {
-        "id": 243,
-        "scenario": "John tells Mary 'It's so nice outside'",
-        "question": "What did John want to convey?",
-        "interpretation1": "The weather is nice.",
-        "interpretation2": "The weather is amazing.",
-        "interpretation3": "The weather is bad.",
-        "interpretation4": "The weather is horrible.",
-        "interpretation5": "The weather is ok.",
-    },
-    {
-        "id": 243,
-        "scenario": "John tells Mary 'It's so nice outside'",
-        "question": "What did John want to convey?",
-        "interpretation1": "The weather is nice.",
-        "interpretation2": "The weather is amazing.",
-        "interpretation3": "The weather is bad.",
-        "interpretation4": "The weather is horrible.",
-        "interpretation5": "The weather is ok.",
-    },
-    {
-        "id": 243,
-        "scenario": "John tells Mary 'It's so nice outside'",
-        "question": "What did John want to convey?",
-        "interpretation1": "The weather is nice.",
-        "interpretation2": "The weather is amazing.",
-        "interpretation3": "The weather is bad.",
-        "interpretation4": "The weather is horrible.",
-        "interpretation5": "The weather is ok.",
-    }
+    [
+        {
+            "id": 243,
+            "scenario": "John tells Mary 'It's so nice outside'",
+            "question": "What did John want to convey?",
+            "interpretation1": "The weather is nice.",
+            "interpretation2": "The weather is amazing.",
+            "interpretation3": "The weather is bad.",
+            "interpretation4": "The weather is horrible.",
+            "interpretation5": "The weather is ok.",
+        },
+        {
+            "id": 243,
+            "scenario": "John tells Mary 'It's so nice outside'",
+            "question": "What did John want to convey?",
+            "interpretation1": "The weather is nice.",
+            "interpretation2": "The weather is amazing.",
+            "interpretation3": "The weather is bad.",
+            "interpretation4": "The weather is horrible.",
+            "interpretation5": "The weather is ok.",
+        },
+        {
+            "id": 243,
+            "scenario": "John tells Mary 'It's so nice outside'",
+            "question": "What did John want to convey?",
+            "interpretation1": "The weather is nice.",
+            "interpretation2": "The weather is amazing.",
+            "interpretation3": "The weather is bad.",
+            "interpretation4": "The weather is horrible.",
+            "interpretation5": "The weather is ok.",
+        }
+    ]
 ]
