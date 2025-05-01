5a4e227f83081c9f39b253ca3228887e69e5c2a8
cesare-spinoso <cesare.spinoso@gmail.com>
2025-05-01 16:29:50 -0400

    update again

--- pilot_study_1/js/quality_checks.js
+++ pilot_study_1/js/quality_checks.js
@@ -2,8 +2,8 @@
 
 var quality_checks = [
     {
-        "scenario": "Alice and Bob are hanging out at Bob's house. Alice asks Bob",
-        "question": "Can you turn on the heater?",
+        "scenario": "Alice and Bob are hanging out at Bob's house. Alice asks Bob \"Can you turn on the heater?\"",
+        "question": "What did Alice want to convey?",
         "interpretations": [
             "The room is freezing.",
             "The room is cool.",
