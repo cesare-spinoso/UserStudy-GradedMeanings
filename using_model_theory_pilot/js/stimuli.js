474d92c2b0dd99a9016519e52360348bb3a404c7
cesare-spinoso <cesare.spinoso@gmail.com>
2025-05-01 14:54:28 -0400

    test with two lists

--- using_model_theory_pilot/js/stimuli.js
+++ using_model_theory_pilot/js/stimuli.js
@@ -32,5 +32,37 @@ var all_stims = [
             "interpretation4": "The weather is horrible.",
             "interpretation5": "The weather is ok.",
         }
+    ],
+    [
+        {
+            "id": 243,
+            "scenario": "John tells Mary \"It's so nice outside.\"",
+            "question": "What did John want to convey?",
+            "interpretation1": "The weather is nice.",
+            "interpretation2": "The weather is amazing.",
+            "interpretation3": "The weather is bad.",
+            "interpretation4": "The weather is horrible.",
+            "interpretation5": "The weather is ok.",
+        },
+        {
+            "id": 243,
+            "scenario": "John tells Mary \"It's so nice outside.\"",
+            "question": "What did John want to convey?",
+            "interpretation1": "The weather is nice.",
+            "interpretation2": "The weather is amazing.",
+            "interpretation3": "The weather is bad.",
+            "interpretation4": "The weather is horrible.",
+            "interpretation5": "The weather is ok.",
+        },
+        {
+            "id": 243,
+            "scenario": "John tells Mary \"It's so nice outside.\"",
+            "question": "What did John want to convey?",
+            "interpretation1": "The weather is nice.",
+            "interpretation2": "The weather is amazing.",
+            "interpretation3": "The weather is bad.",
+            "interpretation4": "The weather is horrible.",
+            "interpretation5": "The weather is ok.",
+        }
     ]
 ]
