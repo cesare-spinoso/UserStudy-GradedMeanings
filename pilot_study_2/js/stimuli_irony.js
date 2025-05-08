d25696db09b596b33e234e57a538a757a46d8d7d
cesare-spinoso <cesare.spinoso@gmail.com>
2025-05-07 20:19:03 -0400

    save stim

--- pilot_study_2/js/stimuli_irony.js
+++ pilot_study_2/js/stimuli_irony.js
@@ -1,61 +1,59 @@
 // all the stimuli data which comes from src/data/original_prag_mega/IronyMergedRawPivotedSample5.csv (does not include quality checks) 
 
 var stimuli_irony = [
-    [
-        {
-            "id": 17,
-            "scenario": "While Tom and a new acquaintance from work, Sara, were chatting at a party, they noticed a colleague across the room. She was standing alone holding a drink and a CD. Tom points at the girl and comments:  \"The life of the party, right there.\"",
-            "question": "What did Tom want to convey?",
-            "interpretations": [
-                "Their colleague is very sociable.",
-                "Their colleague is quite unsociable.",
-                "Their colleague has good taste in music.",
-                "Their colleague has good taste in fashion."
-            ]
-        },
-        {
-            "id": 12,
-            "scenario": "Melissa's friend convinces her to accompany her to a nightclub that her friend says is \"super hot\". When they arrive, it is so cold inside that they can see their breath. Melissa turns to her friend and says, \"The heat is really on in here.\"",
-            "question": "What did Melissa want to convey?",
-            "interpretations": [
-                "It is too hot.",
-                "It is too cold.",
-                "It is too bright.",
-                "It is too loud."
-            ]
-        },
-        {
-            "id": 1,
-            "scenario": "In a shop, Lara tries on a dress. The dress is far too long for her. Lara asks Simon: \"Does this dress fit me?\" Simon answers: \"Wow! That must be custom made! It's clearly the perfect size and length for you.\"",
-            "question": "What did Simon want to convey?",
-            "interpretations": [
-                "The dress is fitting well.",
-                "Lara needs to get a dress of a shorter length.",
-                "Simon does not like the color of this dress.",
-                "Simon has to get back to work."
-            ]
-        },
-        {
-            "id": 9,
-            "scenario": "Adrian's mother asks if he can look in his brother Tom's room to check if he has tidied up. Adrian opens the door to Tom's room, peers in, and sees that the room appears as it normally does - cluttered with toys and dirty clothes. He shouts to his mother: \"Mother, Tom, as usual, has done a splendid job tidying up!\"",
-            "question": "What did Adrian want to convey?",
-            "interpretations": [
-                "Tom tidied up his room.",
-                "Tom's room is still messy.",
-                "Tom cannot find a vacuum.",
-                "Tom cannot find a lawyer."
-            ]
-        },
-        {
-            "id": 24,
-            "scenario": "It was the night before her exam and Tilly had read none of the course texts. Her brother said, \"I see your revision is going well.\"",
-            "question": "What did he want to convey?",
-            "interpretations": [
-                "Tilly is ready for the exam.",
-                "Tilly is unready for the exam.",
-                "Tilly cannot find the textbook.",
-                "Tilly cannot find the sneakers."
-            ]
-        }
-    ]
+    {
+        "id": 17,
+        "scenario": "While Tom and a new acquaintance from work, Sara, were chatting at a party, they noticed a colleague across the room. She was standing alone holding a drink and a CD. Tom points at the girl and comments:  \"The life of the party, right there.\"",
+        "question": "What did Tom want to convey?",
+        "interpretations": [
+            "Their colleague is very sociable.",
+            "Their colleague is quite unsociable.",
+            "Their colleague has good taste in music.",
+            "Their colleague has good taste in fashion."
+        ]
+    },
+    {
+        "id": 12,
+        "scenario": "Melissa's friend convinces her to accompany her to a nightclub that her friend says is \"super hot\". When they arrive, it is so cold inside that they can see their breath. Melissa turns to her friend and says, \"The heat is really on in here.\"",
+        "question": "What did Melissa want to convey?",
+        "interpretations": [
+            "It is too hot.",
+            "It is too cold.",
+            "It is too bright.",
+            "It is too loud."
+        ]
+    },
+    {
+        "id": 1,
+        "scenario": "In a shop, Lara tries on a dress. The dress is far too long for her. Lara asks Simon: \"Does this dress fit me?\" Simon answers: \"Wow! That must be custom made! It's clearly the perfect size and length for you.\"",
+        "question": "What did Simon want to convey?",
+        "interpretations": [
+            "The dress is fitting well.",
+            "Lara needs to get a dress of a shorter length.",
+            "Simon does not like the color of this dress.",
+            "Simon has to get back to work."
+        ]
+    },
+    {
+        "id": 9,
+        "scenario": "Adrian's mother asks if he can look in his brother Tom's room to check if he has tidied up. Adrian opens the door to Tom's room, peers in, and sees that the room appears as it normally does - cluttered with toys and dirty clothes. He shouts to his mother: \"Mother, Tom, as usual, has done a splendid job tidying up!\"",
+        "question": "What did Adrian want to convey?",
+        "interpretations": [
+            "Tom tidied up his room.",
+            "Tom's room is still messy.",
+            "Tom cannot find a vacuum.",
+            "Tom cannot find a lawyer."
+        ]
+    },
+    {
+        "id": 24,
+        "scenario": "It was the night before her exam and Tilly had read none of the course texts. Her brother said, \"I see your revision is going well.\"",
+        "question": "What did he want to convey?",
+        "interpretations": [
+            "Tilly is ready for the exam.",
+            "Tilly is unready for the exam.",
+            "Tilly cannot find the textbook.",
+            "Tilly cannot find the sneakers."
+        ]
+    }
 ];
