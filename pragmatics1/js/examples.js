e7041b28a514c8ce60dfd00b186a122b705d847d
cesare-spinoso <cesare.spinoso@gmail.com>
2025-06-05 15:38:29 -0400

    added example data

---
+++ pragmatics1/js/examples.js
@@ -0,0 +1,27 @@
+// TODO: Make examples which are specific to each pragmatic phenomena
+// OPTIONAL TODO: Can also add example of point values, though this might not be needed
+
+var example_stimuli = [
+    {
+        "id": "example1",
+        "scenario": "This is the example scenario. The character says \"Dear God!\"",
+        "question": "What do you think about this scenario?",
+        "interpretations": [
+            "Interpretation 1 - example 1",
+            "Interpretation 2 - example 1",
+            "Interpretation 3 - example 1",
+            "Interpretation 4 - example 1"
+        ]
+    },
+    {
+        "id": "example2",
+        "scenario": "This is another example scenario. The character says \"What a beautiful day!\"",
+        "question": "Now, do you think this scenario is about God or the Devil?",
+        "interpretations": [
+            "Interpretation 1 - example 2",
+            "Interpretation 2 - example 2",
+            "Interpretation 3 - example 2",
+            "Interpretation 4 - example 2"
+        ]
+    }
+]
