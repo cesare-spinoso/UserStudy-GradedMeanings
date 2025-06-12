cfb83961e8d83aa33971a2525ccbe90da0823ece
cesare-spinoso <cesare.spinoso@gmail.com>
2025-06-12 16:22:32 -0400

    apply to main as well

--- pragmatics1/js/experiment.js
+++ pragmatics1/js/experiment.js
@@ -258,102 +258,23 @@ function make_slides(f) {
 
     start: function () {
       $('.err').hide();
-      this.display_stimulus();
-    },
-
-    display_stimulus: function () {
-      if (this.index < exp.stimuli.length) {
-        const stim = exp.stimuli[this.index];
-
-        // Set the scenario and question
-        const scenario = stim.scenario;
-        // Add <strong>Scenario:</strong> to the beginning of the scenario
-        const scenarioWithLabel = `<strong>Scenario:</strong> ${scenario}`;
-        // Make the utterance green so it stands out
-        const highlighted = scenarioWithLabel.replace(/\"(.*?)\"/, '\"<span style="color: #318500;">$1</span>\"');
-        // Bold the question
-        const question = `<strong>${stim.question}</strong>`;
-        $("#trial-scenario").html(highlighted);
-        $("#trial-question").html(question);
-
-        // Set the interpretations
-        for (let i = 0; i < stim.interpretations.length; i++) {
-          $(`#interp${i + 1}`).text(stim.interpretations[i]);
-        }
-
-        $(".alloc").val("");
-        $("#point-total").text("0");
-        $("#rationale").val("");
-        $(".err").hide();
-
-        $(".alloc").off("input").on("input", () => {
-          const result = validateAllocations("main");
-          $("#point-total").text(result.total);
-
-          if (!result.valid) {
-            $(".err").text(result.errMsg).show();
-          } else if (result.total !== 100) {
-            $(".err").text("Total must equal 100.").show();
-          } else {
-            $(".err").hide();
-          }
-        });
-
-        // Automatically place cursor in first input
-        $(".alloc").first().focus();
-
-      } else {
-        exp.go();
-      }
+      console.log(this.index);
+      console.log(exp.stimuli);
+      console.log("In the start of main slide");
+      this.display_stimulus(current_index = this.index, stimuli = exp.stimuli, stimuli_type = "main");
     },
 
     button: function () {
-      $(".err").hide();
-      const rationale = $("#rationale").val().trim();
-
-      if (!rationale) {
-        $(".err").text("Please provide a rationale.").show();
-        return;
-      }
-
-      const result = validateAllocations("main");
-      $("#point-total").text(result.total);
-
-      if (!result.valid) {
-        $(".err").text(result.errMsg).show();
-        return;
-      } else if (result.total !== 100) {
-        $(".err").text("Total must equal 100.").show();
-        return;
-      }
-
-      this.log_responses(rationale, result.inputs);
+      console.log(this.index);
+      console.log(exp.stimuli);
+      console.log("In the button of main slide");
+      trial_button_event(current_index = this.index, stimuli_type = "main", stimuli = exp.stimuli);
       this.index++;
-      this.display_stimulus();
+      this.display_stimulus(current_index = this.index, stimuli = exp.stimuli, stimuli_type = "main");
     },
-
-    log_responses: function (rationale, inputs) {
-      const stim = exp.stimuli[this.index];
-
-      let trial_data = {
-        id: stim.id,
-        condition_index: exp.condition_index,
-        scenario: stim.scenario,
-        question: stim.question,
-        rationale: rationale,
-        time_in_minutes: (Date.now() - exp.startT) / 60000
-      };
-
-      // Add interpretations and allocations with a loop
-      for (let i = 1; i <= stim.interpretations.length; i++) {
-        trial_data[`interpretation${i}`] = stim.interpretations[i - 1];
-        trial_data[`allocation${i}`] = inputs[i - 1];
-      }
-
-      exp.data_trials.push(trial_data);
-
-    }
   });
+
+  // ADD INFO SLIDE //
 
   slides.add_info = slide({
     name: "add_info",
@@ -365,9 +286,7 @@ function make_slides(f) {
         comments: $("#comments").val() || "NA"
       };
       exp.data = {
-        "warmup_trials": exp.warmup_trials,
-        "trials": exp.data_trials,
-        "catch_trials": exp.catch_trials,
+        "collected_data": exp.collected_data,
         "additional_information": exp.add_data
       };
       proliferate.submit(exp.data);
