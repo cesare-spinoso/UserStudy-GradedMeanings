c1224024034ea48be3e34a0e87b87da2449281fa
cesare-spinoso <cesare.spinoso@gmail.com>
2025-06-25 21:39:13 -0400

    update time

--- pragmatics2/js/alternative_instructions.js
+++ pragmatics2/js/alternative_instructions.js
@@ -1,4 +1,4 @@
-var alternative_instructions = "<strong>Alternative utterance</strong>: In addition, in these scenarios, you will be shown an alternative utterance, that is something the character <em>could have said</em> but <em>chose not to</em>. You should consider this alternative utterance when coming up with your interpretation (i.e., point distribution) for the character's statement.";
+var alternative_instructions = "<strong>Alternative utterance</strong>: In addition, in these scenarios, you will be shown an <strong>alternative utterance:</strong> something the character <em>could have said</em> but <em>chose not to</em>. You should consider this alternative utterance when coming up with your interpretation (i.e., point distribution) for the character's statement.";
 
 function alternative_utterance_html(speaker_name, alternative_utterance) {
     return `In addition, assume that ${speaker_name} <em>could have but chose not to say</em> \"<span style="color: #318500;">${alternative_utterance}</span>\"`;
